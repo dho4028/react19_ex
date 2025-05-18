5375e50cb403cc509754e85d425027aa8cbb51ab
dho4028 <dho4028@naver.com>
2025-05-18 11:01:07 +0900

    영화포스터, 제목, tailwindCss4 적용

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import MovieList from './pages/MovieList';
+function App() {
+	return (
+		<>
+			<h1>TailwindCSS 4</h1>
+			<MovieList />
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,15 @@
+export default function Card({ data }) {
+	return (
+		<article className='relative mb-16 w-65 h-100'>
+			<a href={'/' + data.id}>
+				<img
+					src={`https://image.tmdb.org/t/p/w200${data.poster_path}`}
+					alt={data.title}
+				/>
+			</a>
+			<h2 className='mt-3 text-3xl font-dongle'>
+				<a href={'/' + data.id}>{data.title}</a>
+			</h2>
+		</article>
+	);
+}
